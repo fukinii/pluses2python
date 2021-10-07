20f0c33ecb4794adc1fd6e457708919ef4b4c89a
fukinii <fookinilia2013@gmail.com>
2021-10-07 16:52:59 +0300

    Добавление строк в gitignore

---
+++ pluses/cmake-build-debug/src/CMakeFiles/pluses2python.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pluses2python.dir/Add.cpp.o"
+  "CMakeFiles/pluses2python.dir/main.cpp.o"
+  "libpluses2python.a"
+  "libpluses2python.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pluses2python.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pluses/cmake-build-debug/src/CMakeFiles/pluses2python.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libpluses2python.a"
+)
