5338956abfc5159f9b36fa2bd993dcab8f107a0c
fukinii <fookinilia2013@gmail.com>
2021-10-08 20:09:54 +0300

    Добвление файлов под Джини

---
+++ pluses/src/utils/CalcGini.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fukin on 08.10.2021.
+//
+
+#include "CalcGini.h"
---
+++ pluses/src/utils/CalcGini.h
@@ -0,0 +1,8 @@
+//
+// Created by fukin on 08.10.2021.
+//
+
+#ifndef PLUSESTOPYTHON_CALCGINI_H
+#define PLUSESTOPYTHON_CALCGINI_H
+
+#endif //PLUSESTOPYTHON_CALCGINI_H
